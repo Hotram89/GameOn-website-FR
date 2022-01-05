642554dd33c59e4e296807ba33ff4f84d66bf8a7
Hotram89 <marion.dournel@gmail.com>
2022-01-05 12:23:29 +0100

    projet avec le module JS

---
+++ starterOnly/scripts/logique_metier.js
@@ -0,0 +1,36 @@
+// ajouter une logique métier permet de garantir 
+// que les colonnes d’une application pilotée par modèle 
+// sont affichées, masquées ou définies avec les valeurs correctes.
+
+// DOM Elements
+const closeForm = document.getElementById("cross");
+const modalbg = document.getElementById("formWindow");
+const modalBtn = document.querySelectorAll(".modal-btn");
+
+// launch modal function
+export function launchModal() {
+    modalbg.style.display = "block";
+  }
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));  
+  
+// close modal event
+
+
+closeForm.addEventListener("click", () => {
+  const description = document.querySelector(".hero-text");
+    modalbg.style.display = "none";
+    description.parentNode.removeChild(description);
+
+    console.log(description);
+  console.log(typeof description);
+  }) 
+  
+ 
+
+  
+
+  //
+  
+
+  
